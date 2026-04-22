0374305547db2e9630e5155c540452f243d810c0
Preeti Sharma <36129347+preeti13456@users.noreply.github.com>
2026-04-22 14:12:12 +0530

    Add: invert-a-binary-tree - submission-0

---
+++ Data Structures & Algorithms/invert-a-binary-tree/submission-0.cpp
@@ -0,0 +1,37 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+
+class Solution {
+public:
+    TreeNode* invertTree(TreeNode* root) {
+        //if we visit every node exactly once 
+        //swap left and right children , the whole tree inverted
+        //queue to process nodes level by level
+            queue<TreeNode*>q;
+            if(!root) return nullptr;
+
+            q.push(root);
+
+            while(!q.empty())
+            {
+                TreeNode * node = q.front();
+                q.pop();
+                swap(node->left, node->right);
+                if(node->left)q.push(node->left);
+                if(node->right)q.push(node->right);
+
+            }
+return root;
+
+
+    }
+};
