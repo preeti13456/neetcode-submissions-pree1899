1c116bf6fd49596c9654cf7047ebe3a458aa7b0a
Preeti Sharma <36129347+preeti13456@users.noreply.github.com>
2026-04-24 00:29:40 +0530

    Add: burst-balloons - submission-0

---
+++ Data Structures & Algorithms/burst-balloons/submission-0.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+int solve(int l, int r, int leftBoundary, int rightBoundary, vector<int>&nums, vector<vector<int>>&dp)
+{
+    if(l>r) return 0;
+    int maxCoins = 0;
+    if(dp[l][r]!=-1)
+    return dp[l][r];
+
+    for(int i=l; i<=r; i++)
+    {
+        int coins = leftBoundary*nums[i]*rightBoundary;
+        maxCoins = max(maxCoins, coins + solve(l, i-1, leftBoundary, nums[i], nums, dp)+solve(i+1, r, nums[i], rightBoundary, nums, dp));
+
+    }
+    return dp[l][r] = maxCoins;
+}
+    int maxCoins(vector<int>& nums) {
+        //0.->[4,2,3,7]
+
+        //1. -> [4, 3, 7] = 4
+        //2, -> [ 4, 7]
+        //3.->[ 7]
+        //4.->[]
+     int l = 0, r=nums.size()-1;
+     int n = nums.size();
+     vector<vector<int>>dp(n, vector<int>(n , -1));
+     return solve(l, r, 1, 1, nums, dp);
+    }
+};
